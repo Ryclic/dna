a81a6b55361e2bbee4b2c34dc72a4918e5843901
Ryan Chen <ryanchen175@gmail.com>
2021-08-01 13:52:56 -0700

    formatted input files

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-function show(name){
-    console.log("Hello, " + name)
+const dnaSeq = 
+
+const markers = [
+
+]
+
+function findPattern(dnaSeq, markers){
+    var matches = [];
+    for(let j = 0; j < markers.length; j++){
+        for(let i = 0; i < dnaSeq.length-3; i++){
+            if(dnaSeq.substring(i, i+3) == markers[j].marker){
+                matches.push({name: markers[j].name, marker: markers[j].marker, foundAt: i});
+            }
+        }
+    }
+    return matches;
+
 }
 
-function goodbye(name){
-    console.log("Goodbye, " + name)
-}
-
-show('Ryan')
+console.log(findPattern(dnaSeq, markers));
